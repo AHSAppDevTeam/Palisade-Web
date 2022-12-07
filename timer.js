208d6eac9e009f2b78ba7e778ecf72771818b5b0
ekam09 <72772827+ekam09@users.noreply.github.com>
2022-12-06 16:10:50 -0800

    test

--- timer.js
+++ timer.js
@@ -1,118 +1,94 @@
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = 0;
-            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
-        }
-    }, 1000);
-}
-
-window.onload = function () {
-    var time = 60 / 2, // your time in seconds here
-        display = document.querySelector('#safeTimerDisplay');
-    startTimer(time, display);
-};
-
 class Timer {
-
-    constructor() {
-        this.time = 0;
-        this.element = null;
-        this.control = true;
-        this.callback = null;
-        this.timeLimit = 10;
-    }
-
-    set(time, id, callback = null) {
-        this.time = time;
-        this.element = document.getElementById(id);
-        this.callback = callback;
-    }
-
-    setTimeLimit(time) {
-        this.timeLimit = time;
-    }
-
-    start(type = 'COUNT_DOWN') {
-        this.control = true;
-
-        setTimeout(() => {
-            if(type == 'COUNT_DOWN')
-                this.countDown();
-            else if(type == 'COUNT_UP') 
-                this.countUp();
-        }, 1000); }
-
-        format() {
-            let hours = parseInt(this.time / 3600);
-            let timeLeft = this.time - hours * 3600;
-            let minutes = parseInt(timeLeft / 60);
-            timeLeft = timeLeft - minutes * 60;
-            let seconds = timeLeft;
-            
-            hours = hours.toString();
-            minutes = minutes.toString();  seconds = seconds.toString();
-    
-            if (hours.length == 1)
-                hours = '0' + hours;
-            if (minutes.length == 1)
-                minutes = '0' + minutes;
-            if (seconds.length == 1)
-                seconds = '0' + seconds;
-            
-            return hours + ':' + minutes + ':' + seconds;
+    constructor(root) {
+      root.innerHTML = Timer.getHTML();
+  
+      this.el = {
+        minutes: root.querySelector(".timer__part--minutes"),
+        seconds: root.querySelector(".timer__part--seconds"),
+        control: root.querySelector(".timer__btn--control"),
+        reset: root.querySelector(".timer__btn--reset")
+      };
+  
+      this.interval = null;
+      this.remainingSeconds = 0;
+  
+      this.el.control.addEventListener("click", () => {
+        if (this.interval === null) {
+          this.start();
+        } else {
+          this.stop();
         }
-    
-        countDown() {
-            if(!this.control)
-                return;
-            let timerblock = this.element;
-            timerblock.innerHTML = this.format();
-            timerblock.style.display = 'block';
-    
-            if (this.time <= 59) {
-                timerblock.style.color = 'red';
-            }if (this.time <= 0) {
-                timerblock.innerHTML = 'Time end!';
-                this.callback();
-                this.stop();
-            }
-            else {
-                setTimeout(() => {
-                    this.countDown();
-                }, 1000);
-                this.time--;
-            }
+      });
+  
+      this.el.reset.addEventListener("click", () => {
+        const inputMinutes = prompt("Enter number of minutes:");
+  
+        if (inputMinutes < 60) {
+          this.stop();
+          this.remainingSeconds = inputMinutes * 60;
+          this.updateInterfaceTime();
         }
-    
-        countUp() {
-            if(!this.control)
-                return;
-            let timerblock = this.element;
-            timerblock.innerHTML = this.format(); timerblock.style.display = 'block';
-    
-            if(this.time >= this.timeLimit) {
-                timerblock.innerHTML = 'Timer Limit Exceed!';
-                this.callback();
-                this.stop();
-            }
-            else {
-                setTimeout(() => {
-                    this.countUp();
-                }, 1000);
-                this.time++;
-            }
-        }stop() {
-            this.control = false;
-        }
+      });
     }
-    
+  
+    updateInterfaceTime() {
+      const minutes = Math.floor(this.remainingSeconds / 60);
+      const seconds = this.remainingSeconds % 60;
+  
+      this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+      this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+    }
+  
+    updateInterfaceControls() {
+      if (this.interval === null) {
+        this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+        this.el.control.classList.add("timer__btn--start");
+        this.el.control.classList.remove("timer__btn--stop");
+      } else {
+        this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+        this.el.control.classList.add("timer__btn--stop");
+        this.el.control.classList.remove("timer__btn--start");
+      }
+    }
+  
+    start() {
+      if (this.remainingSeconds === 0) return;
+  
+      this.interval = setInterval(() => {
+        this.remainingSeconds--;
+        this.updateInterfaceTime();
+  
+        if (this.remainingSeconds === 0) {
+          this.stop();
+        }
+      }, 1000);
+  
+      this.updateInterfaceControls();
+    }
+  
+    stop() {
+      clearInterval(this.interval);
+  
+      this.interval = null;
+  
+      this.updateInterfaceControls();
+    }
+  
+    static getHTML() {
+      return `
+              <span class="timer__part timer__part--minutes">00</span>
+              <span class="timer__part">:</span>
+              <span class="timer__part timer__part--seconds">00</span>
+              <button type="button" class="timer__btn timer__btn--control timer__btn--start">
+                  <span class="material-icons">play_arrow</span>
+              </button>
+              <button type="button" class="timer__btn timer__btn--reset">
+                  <span class="material-icons">timer</span>
+              </button>
+          `;
+    }
+  }
+  
+  new Timer(
+      document.querySelector(".timer")
+  );
